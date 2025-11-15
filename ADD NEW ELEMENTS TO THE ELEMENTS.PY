38a132da5a3fdac02fcebb51dc4ff8a470c86d5b
Vinyasa <vinyasank@gmail.com>
2025-11-15 13:09:51 +0530

    Create ADD NEW ELEMENTS TO THE ELEMENTS.PY

---
+++ ADD NEW ELEMENTS TO THE ELEMENTS.PY
@@ -0,0 +1,6 @@
+#add new elements to the end of the list
+my_list = [10,20,30]
+my_list.append(40)
+print(f"list after appending: {my_list}")
+my_list.remove(20)
+print(f"list after removing 20: {my_list}")
