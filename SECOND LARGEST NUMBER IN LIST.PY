b915451c205d0df0141c8486b59979e68d9bd752
Vinyasa <vinyasank@gmail.com>
2025-11-15 13:12:45 +0530

    Create SECOND LARGEST NUMBER IN LIST.PY

---
+++ SECOND LARGEST NUMBER IN LIST.PY
@@ -0,0 +1,5 @@
+# find second largest number in the list
+my_list = [10, 20, 30, 40, 50]
+my_list.sort()
+second_largest = my_list[-2]
+print("The second largest number in the list is:", second_largest)
